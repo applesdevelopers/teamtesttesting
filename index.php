5aaceec27c9cd6e9f709a1ba4ea58257812769d6
Prateek Varshney <varsh.prateek@gmail.com>
2017-05-17 15:06:47 +0530

    first

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+try {
+  $customer_id = "..."; // Load the Stripe Customer ID for your logged in user
+  $customer = \Stripe\Customer::retrieve($customer_id);
+  header('Content-Type: application/json');
+  echo $customer->jsonSerialize();
+} catch(\Stripe\Error $e) {
+  http_response_code(402);
+}
+*/
+
+try {
+  
+  $customer = '{"data" : "Hello"}';
+  header('Content-Type: application/json');
+  echo $customer->jsonSerialize();
+} catch(\Stripe\Error $e) {
+  http_response_code(402);
+}
